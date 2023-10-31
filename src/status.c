a5d6a261c60f67d8c5eb38491ed84099cc457fee
adliarindra <niterix27@gmail.com>
2023-10-31 22:31:52 +0700

    status v1

---
+++ src/status.c
@@ -0,0 +1,31 @@
+#include "status.h"
+#include "ADT/Queue/circular_queue.h"
+
+void status(Queue queue, char* current_playlist){
+    if (IsEmpty(queue))
+    {
+        printf("Now Playing:\nNo songs have been played yet. Please search for a song to begin playback.\n\nQueue:\nYour queue is empty.\n");
+    }
+    else if (Length(queue) == 1)
+    {
+        printf("Now Playing:\n%s\n\nQueue:\nYour queue is empty.\n", HEAD(queue));
+    }
+    else if (current_playlist[0] == "\0")
+    {
+        printf("Now Playing:\n%s\n\nQueue:\n", HEAD(queue));
+        for (int i = queue.idxHead + 1; i <= queue.idxTail; ++i)
+        {
+            printf("%d. %s\n", i - queue.idxHead, queue.Tab[i]);
+        }
+    }
+    else
+    {
+        printf("Current Playlist: %s\n", current_playlist);
+        printf("Now Playing:\n%s\n\nQueue:\n", HEAD(queue));
+        for (int i = queue.idxHead + 1; i <= queue.idxTail; ++i)
+        {
+            printf("%d. %s\n", i - queue.idxHead, queue.Tab[i]);
+        }
+    }
+
+}
---
+++ src/status.h
@@ -0,0 +1,12 @@
+#ifndef __status__
+#define __status__
+
+void status(Queue queue, char* current_playlist);
+/* Menerima daftar lagu dalam queue dalam argumen queue, dan nama playlist yang diputar
+   dalam argumen current_playlist. Bila queue sekarang bukan dari playlist maka argumen
+   current_playlist yang dimasukkan adalah string kosong ("") 
+   I.S. Queue terdefinisi
+   F.S. Menampilkan status sesuai spesifikasi
+   */
+
+#endif __status__;
