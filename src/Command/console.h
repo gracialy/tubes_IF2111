fbb7e35987b1f08e52f15816b790e676ae8dbcaf
silalahi20 <josiasilalahi2004@gmail.com>
2023-10-31 21:18:38 +0700

    tes dulu ga si

---
+++ src/Command/console.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "../boolean.h"
+#include "../ADT/list/list.h"
+#include "../ADT/Map/map.h"
+#include "../ADT/Mesin Karakter/mesinkarakter.h"
+#include "../ADT/Mesin Kata/mesinkata.h"
+#include "../ADT/Queue/circular_queue.h"
+#include "../ADT/Stack/stack.h"
+
+
+#ifndef HEADER_H
+#define HEADER_H
+
+// Daftar Header Command
+
+
+
+#endif
