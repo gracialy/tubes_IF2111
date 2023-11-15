7d0d6a532bb33f5183457ef4e62ab8e336669694
silalahi20 <josiasilalahi2004@gmail.com>
2023-11-15 16:49:43 +0700

    driver queue

--- src/ADT/Queue/driver_queue.c
+++ src/ADT/Queue/driver_queue.c
@@ -5,11 +5,18 @@ int main(){
     Queue Q1,Q2;
     CreateQueue(&Q1);
     CreateQueue(&Q2);
-    for (int i = 1; i <= 100; ++i) {
+    while(!IsFull(Q1)){
+        for (int i =1; i <= 100; ++i) {
         enqueue(&Q1, i);
+        }
     }
-    if (IsFull(Q1)) {
-        printf("Q1 penuh");
+    for (int i=1;i <= 49;i++){
+            dequeue(&Q1);
+            enqueue(&Q2, i);
     }
-
+    displayQueue(Q1);
+    int a = Length(Q1);
+    displayQueue(Q2);
+    int b = Length(Q2);
+    printf("Banyak elemen pada Q1 adalah %d dan Banyak elemen pada Q2 adalah %d", a,b);
 }
