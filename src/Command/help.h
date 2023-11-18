806dbcc4cc8d47bf2896810a172f3c9d7d21129e
silalahi20 <josiasilalahi2004@gmail.com>
2023-11-18 19:57:53 +0700

    help

---
+++ src/Command/help.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#ifndef __HELP__
+#define __HELP__
+
+void helpBeforeLoad();
+/* I.S. sembarang */
+/* F.S. mengembalikan output berupa daftar command yang dapat dimasukkan berikut fungsinya */
+
+void helpAfterLoad();
+/* I.S. sembarang */
+/* F.S. mengembalikan output berupa daftar command yang dapat dimasukkan berikut fungsinya */
+
+#endif
