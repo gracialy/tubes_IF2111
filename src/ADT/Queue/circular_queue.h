fb317f0dc2e9d243edae603cc6a60f71b918902d
gracialy <ly.gracies@gmail.com>
2023-11-18 20:47:29 +0700

    change int queue to char queue

--- src/ADT/Queue/circular_queue.h
+++ src/ADT/Queue/circular_queue.h
@@ -6,16 +6,17 @@
 #define __circular_queue_h__
 
 #include "../../Command/boolean.h"
+#include "../Mesin_Kata/mesinkata.h"
 
 #define IDX_UNDEF -1
 #define IDX_MAX 99
 
 /* Definisi tipe elemen dan indeks pada Queue */
-typedef int ElType;
+typedef char ElType;
 typedef int IdxType;
 
 typedef struct {
-        ElType Tab[IDX_MAX+1];  /* tabel penyimpan elemen */
+        ElType Tab[IDX_MAX+1][NMax];  /* tabel penyimpan elemen */
         IdxType idxHead;  /* indeks elemen paling awal (terdepan) */
         IdxType idxTail;  /* indeks tempat menambah elemen baru */
 } Queue;
@@ -28,13 +29,13 @@ typedef struct {
 #define     TAIL(q) (q).Tab[(q).idxTail]
 
 /* ********* Prototype ********* */
-boolean IsEmpty (Queue Q);
+boolean IsEmptyQ (Queue Q);
 /* Mengirim true jika Q kosong */
 /* yaitu ketika idxHead=IDX_UNDEF dan idxTail=IDX_UNDEF */
-boolean IsFull (Queue Q);
+boolean IsFullQ (Queue Q);
 /* Mengirim true jika tabel penampung elemen Q sudah penuh */
 /* yaitu ketika idxHead=0 dan idxTail=IDX_MAX atau idxHead=idxTail+1 ketika idxHead > idxTail */
-int Length (Queue Q);
+int LengthQ (Queue Q);
 /* Mengirimkan banyaknya elemen Q, 0 jika kosong */
 
 /* *** Kreator *** */
@@ -45,13 +46,13 @@ void CreateQueue (Queue * Q);
 /* - idxTail=IDX_UNDEF. */
 
 /* *** Primitif Add/Delete *** */
-void enqueue (Queue * Q, ElType X);
+void enqueue (Queue * Q, ElType* X);
 /* Proses: Menambahkan X pada Q dengan aturan FIFO */
 /* I.S. Q mungkin kosong, tabel penampung elemen Q TIDAK penuh */
 /* F.S. Tail "maju" dengan mekanisme circular buffer,
         X menjadi idxTail yang baru
         Jika Q kosong, idxHead dimulai dari 0 */
-ElType dequeue (Queue * Q);
+void dequeue (Queue * Q, char* target);
 /* Proses: Menghapus idxHead pada Q dengan aturan FIFO, lalu mengembalikan nilainya */
 /* I.S. Q tidak mungkin kosong */
 /* F.S. mengembalikan nilai Q pada idxHead;
@@ -67,5 +68,6 @@ void displayQueue(Queue q);
 /* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
 /* Jika Queue kosong : menulis [] */
 /* Note: Output mengandung newline */
+
 
 #endif
