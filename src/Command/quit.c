e47f11d29aad6245edb90c1ab8055766edb4db92
silalahi20 <josiasilalahi2004@gmail.com>
2023-11-21 01:54:06 +0700

    Quit (sementara dulu)

---
+++ src/Command/quit.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "quit.h"
+
+void Quit(){
+    printf("\nAnda keluar dari WayangWave\n See u when I see u ^_^ ...\n");
+    exit(0);
+}
---
+++ src/Command/quit.h
@@ -0,0 +1,8 @@
+#ifndef _QUIT_
+#define _QUIT_
+
+void Quit();
+// I.S : Sembarang
+//F.S : Akan keluar dari program WayangWave
+
+#endif
