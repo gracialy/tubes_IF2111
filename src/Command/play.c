7ae3440269ea71975b8c300c2f626873b0e10f0e
adliarindra <niterix27@gmail.com>
2023-11-20 18:52:40 +0700

    play ama playlist

---
+++ src/Command/play.c
@@ -0,0 +1,32 @@
+#include "play.h"
+
+void playSong (char* currentSong, char listArtis[100][50], Map listAlbum, Queue* songQueue, char* currentPlaylist){
+    SelectSong(listArtis, listAlbum, currentSong);
+    stringCopy(currentPlaylist, "\0");
+    CreateQueue(songQueue);
+}
+
+void playPlaylist(char* currentSong, Queue* songQueue, List listPlaylist, Stack* songHistory, char* currentPlaylist){
+    showPlaylist(listPlaylist);
+    printf("Masukkan ID playlist : ");
+    ADVWORD();
+    int idx = stringToInt(currentWord.TabWord) - 1;
+    if (idx < 0 || idx > listPlaylist.Neff)
+    {
+        printf("ID yang dimasukkan tidak ada\n");
+        return;
+    }
+    CreateQueue(songQueue);
+    CreateEmptySt(songHistory);
+    addressLL p = listPlaylist.A[idx].First;
+    for (int i = 0; i < NbElmt(listPlaylist.A[idx]); ++i)
+    {
+        if (i == 0) stringCopy(currentSong, p->info);
+        else
+        {
+            enqueue(songQueue, p->info);
+            Push(songHistory, p->info);
+        }
+        p = p->next;
+    }
+}
