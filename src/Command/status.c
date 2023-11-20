8358b5836dc976c238fc494ed3566e1f75ff644e
silalahi20 <josiasilalahi2004@gmail.com>
2023-11-20 17:02:50 +0700

    update minor status

--- src/Command/status.c
+++ src/Command/status.c
@@ -1,30 +1,45 @@
 #include "status.h"
-#include "../ADT/Queue/circular_queue.h"
 
-void status(Queue queue, char* current_playlist){
-    if (IsEmpty(queue))
+void status(Queue queue, char* current_playlist, char* currentSong, Map listAlbum){
+    if (currentSong[0] == '\0')
     {
-        printf("Now Playing:\nNo songs have been played yet. Please search for a song to begin playback.\n\nQueue:\nYour queue is empty.\n");
+        printf("Now Playing:\nNo songs have been played yet. Please search for a song to begin playback.\n");
     }
-    else if (Length(queue) == 1)
+    else
     {
-        printf("Now Playing:\n%s\n\nQueue:\nYour queue is empty.\n", HEAD(queue));
+        printf("Now Playing:\n%s\n", currentSong);
     }
-    else if (current_playlist[0] == "\0")
+    if (current_playlist[0] == '\0')
     {
-        printf("Now Playing:\n%s\n\nQueue:\n", HEAD(queue));
-        for (int i = queue.idxHead + 1; i <= queue.idxTail; ++i)
+        printf("Queue:\n", HEAD(queue));
+        if (IsEmptyQ(queue)) 
         {
-            printf("%d. %s\n", i - queue.idxHead, queue.Tab[i]);
+            printf("Your queue is empty.\n");
+            return;
+        }
+        for (int i = queue.idxHead; i <= queue.idxTail; ++i)
+        {
+            char artist[NMax], album[NMax];
+            getArtist(queue.Tab[i % (IDX_MAX + 1)], artist, listAlbum);
+            getAlbum(queue.Tab[i % (IDX_MAX + 1)], album, listAlbum);
+            printf("%d. %s - %s - %s\n", i - queue.idxHead + 1, artist, album, queue.Tab[i]);
         }
     }
     else
     {
         printf("Current Playlist: %s\n", current_playlist);
-        printf("Now Playing:\n%s\n\nQueue:\n", HEAD(queue));
-        for (int i = queue.idxHead + 1; i <= queue.idxTail; ++i)
+        printf("Queue:\n", HEAD(queue));
+        if (IsEmptyQ(queue)) 
         {
-            printf("%d. %s\n", i - queue.idxHead, queue.Tab[i]);
+            printf("Your queue is empty.\n");
+            return;
+        }
+        for (int i = queue.idxHead; i <= queue.idxTail; ++i)
+        {
+            char artist[NMax], album[NMax];
+            getArtist(queue.Tab[i % (IDX_MAX + 1)], artist, listAlbum);
+            getAlbum(queue.Tab[i % (IDX_MAX + 1)], album, listAlbum);
+            printf("%d. %s - %s - %s\n", i - queue.idxHead + 1, artist, album, queue.Tab[i]);
         }
     }
 
