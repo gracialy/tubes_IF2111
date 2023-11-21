9f1628951bb4bc595fbb175ab44e3b577da84d2f
gracialy <ly.gracies@gmail.com>
2023-11-22 03:31:50 +0700

    fix all QUEUE command

--- src/Command/queue_song.c
+++ src/Command/queue_song.c
@@ -2,8 +2,11 @@
 
 void queueSong(Map listAlbum, char listArtis[maxA][maxAN], Queue* q){
     char song[NMax];
-    if (SelectSong(listArtis, listAlbum, song)) return;
+    if (SelectSong(listArtis, listAlbum, song)) return; // if song is not found then return
     enqueue(q, song);
+    char artist[NMax];
+    getArtist(song, artist, listAlbum);
+    printf("Berhasil menambahkan lagu \"%s\" oleh \"%s\" ke queue\n", song, artist);
 }
 
 void queuePlaylist(List listPlaylist, Queue* songQueue, char* currentAlbum){
@@ -17,7 +20,7 @@ void queuePlaylist(List listPlaylist, Queue* songQueue, char* currentAlbum){
     printf("Masukkan ID playlist : ");
     ADVWORD();
     int idx = stringToInt(currentWord.TabWord) - 1;
-    if (idx < 0 || idx > listPlaylist.Neff)
+    if (idx < 0 || idx > listPlaylist.Neff-1)
     {
         printf("ID yang dimasukkan tidak ada\n");
         return;
@@ -28,50 +31,68 @@ void queuePlaylist(List listPlaylist, Queue* songQueue, char* currentAlbum){
         enqueue(songQueue, p->info);
         p = p->next;
     }
+    printf("Berhasil menambahkan playlist \"%s\" ke queue\n", listPlaylist.A[idx].Name);
 }
 
 void queueSwap(Queue* songQueue){
     char xstr[NMax], ystr[NMax];
     getWord(2, xstr); getWord(3, ystr);
-    int x = stringToInt(xstr), y = stringToInt(ystr);
-    if (x > y)
+
+    // queue number is not completed
+    if (CompareString(xstr, " ") || CompareString(ystr, " "))
     {
-        int temp = x;
-        x = y;
-        y = temp;
-    }
-    if (x > LengthQ(*songQueue))
-    {
-        printf("ID yang dimasukkan tidak valid!\n");
+        printf("Command yang dimasukkan tidak valid\n");
         return;
     }
 
-    int idx;
-    char tempstr[NMax];
-
-    for (int i = 0; i <= IDX_MAX; ++i)
+    int x = stringToInt(xstr), y = stringToInt(ystr);
+    
+    // queue number is not valid
+    if ((x > LengthQ(*songQueue) || x<=0) && (y > LengthQ(*songQueue) || y<=0))
     {
-        if (i == songQueue->idxHead + x - 1)
-        {
-            idx = i;
-            stringCopy(tempstr, songQueue->Tab[i]);
-        }
-        else if (i == songQueue->idxHead + y - 1)
-        {
-            stringCopy(songQueue->Tab[idx], songQueue->Tab[i]);
-            stringCopy(songQueue->Tab[i], tempstr);
-        }
+        printf("Lagu dengan urutan ke %d dan %d tidak terdapat dalam queue\n", x, y);
+        return;
     }
-    printf("Queue berhasil diswap!\n");
+    else if (x > LengthQ(*songQueue) || x<=0)
+    {
+        printf("Lagu dengan urutan ke %d tidak terdapat dalam queue\n", x);
+        return;
+    }
+    else if (y > LengthQ(*songQueue) || y<=0)
+    {
+        printf("Lagu dengan urutan ke %d tidak terdapat dalam queue\n", y);
+        return;
+    }
 
+    stringCopy(xstr, songQueue->Tab[(songQueue->idxHead + x-1) % (IDX_MAX + 1)]);
+    stringCopy(ystr, songQueue->Tab[(songQueue->idxHead + y-1) % (IDX_MAX + 1)]);
+
+    stringCopy(songQueue->Tab[(songQueue->idxHead + x-1) % (IDX_MAX + 1)], ystr);
+    stringCopy(songQueue->Tab[(songQueue->idxHead + y-1) % (IDX_MAX + 1)], xstr);
+
+    printf("Lagu \"%s\" berhasil ditukar dengan \"%s\"\n", xstr, ystr);
 }
 
 void queueRemove(Queue* songQueue){
     char idxstr[NMax];
     getWord(2, idxstr);
+    // queue number is not completed
+    if (CompareString(idxstr, " "))
+    {
+        printf("Command yang dimasukkan tidak valid\n");
+        return;
+    }
+
     int idx = stringToInt(idxstr);
+    if (idx > LengthQ(*songQueue) || idx<=0)
+    {
+        printf("Lagu dengan urutan ke %d tidak terdapat dalam queue\n", idx);
+        return;
+    }
+
     char temp[IDX_MAX + 1][NMax];
     int n = 0;
+    char artist[NMax];
     while(!IsEmptyQ(*songQueue))
     {
         dequeue(songQueue, temp[n]);
@@ -83,11 +104,16 @@ void queueRemove(Queue* songQueue){
         {
             enqueue(songQueue, temp[i]);
         }
+        else{
+            stringCopy(idxstr, temp[i]);
+            getArtist(idxstr, artist, listAlbum);
+        }
     }
-    printf("Berhasil menghapus lagu dari queue\n");
+    printf("Lagu \"%s\" oleh \"%s\" telah dihapus dari queue!\n", idxstr, artist);
 }
 
 void queueClear(Queue* songQueue){
     char temp[NMax];
     while (!IsEmptyQ(*songQueue)) dequeue(songQueue, temp);
+    printf("Queue berhasil dikosongkan\n");
 }
