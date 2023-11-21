7b55bddf3ce8c7d07e7731cacd982537ce379f22
gracialy <ly.gracies@gmail.com>
2023-11-21 16:37:21 +0700

    feat QUIT including SAVE

--- src/Command/quit.c
+++ src/Command/quit.c
@@ -2,7 +2,15 @@
 #include <stdlib.h>
 #include "quit.h"
 
-void Quit(){
+void Quit(char listArtis[maxA][maxAN], Map* listAlbum, char* currentSong, 
+    Queue* songQueue, Stack* songHistory, List* listPlaylist){
+    printf("Apakah kamu ingin menyimpan data sesi sekarang?");
+    ADVWORD();
+    if (isEqual(-1, "Y"))
+    {
+        save(listArtis[maxA][maxAN], listAlbum, currentSong, 
+        songQueue, songHistory, listPlaylist);
+    }        
     printf("\nAnda keluar dari WayangWave\n See u when I see u ^_^ ...\n");
     exit(0);
 }
--- src/Command/quit.h
+++ src/Command/quit.h
@@ -1,7 +1,10 @@
 #ifndef _QUIT_
 #define _QUIT_
 
-void Quit();
+#include "save.h"
+
+void Quit(char listArtis[maxA][maxAN], Map* listAlbum, char* currentSong, 
+    Queue* songQueue, Stack* songHistory, List* listPlaylist);
 // I.S : Sembarang
 //F.S : Akan keluar dari program WayangWave
 
